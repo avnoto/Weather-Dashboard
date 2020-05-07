ec86e72ac0635fabada66c233e3c68053d981953
avnoto <alexandravnoto@gmail.com>
2020-05-07 16:59:16 -0700

    updated dashboard layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 
-    var APIKey = "83161a29ff65cf40e504ac5568f7c577";
+    doInit();
+
+    function doInit() {
+        currentDay();
+    }
+    
+    function currentDay() {
+        let currentDay = moment().format('MMMM Do YYYY, h:mm a');
+        $("#currentDay").text(currentDay);
+        };
+
+
+    //var APIKey = "83161a29ff65cf40e504ac5568f7c577";
 
 
 
