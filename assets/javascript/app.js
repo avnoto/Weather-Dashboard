66f2e7d167684d3346566da10d974b610ffed84a
avnoto <alexandravnoto@gmail.com>
2020-05-07 18:33:23 -0700

    started API call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,26 +9,41 @@ $(document).ready(function() {
     function currentDay() {
         let currentDay = moment().format('MMMM Do YYYY, h:mm a');
         $("#currentDay").text(currentDay);
-        };
+    }
 
 
-    //var APIKey = "83161a29ff65cf40e504ac5568f7c577";
+    function getWeatherInfo(city) {
+    
+        var APIKey = "83161a29ff65cf40e504ac5568f7c577";
+
+        var city = $("#userInput").val();
+
+        var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+            $.ajax({
+                url: queryURL,
+                method:"GET"  
+            }).then(function(response) {
+                console.log(response);
+
+        
+
+            });
+    }
 
 
+            
 
 
+     $("#sumbitWeather").on("click", function(event) {
 
+        event.preventDefault();
 
+        var inputCity = $("#userInput").val().trim();
 
+        getWeatherInfo(inputCity);
 
-
-
-
-
-
-
-
-
+     });
 
 
 });
